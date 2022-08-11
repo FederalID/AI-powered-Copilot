7b8b45ffc8d6d20c70e75acc43a6745891c373cb
FederalID <117157825+FederalID@users.noreply.github.com>
2022-08-11 07:15:28 +0000

    testin

--- pkg/assistants/simple.go
+++ pkg/assistants/simple.go
@@ -54,4 +54,4 @@ func Assistant(model string, prompts []openai.ChatCompletionMessage, maxTokens i
 	var toolPrompt tools.ToolPrompt
 	if err = json.Unmarshal([]byte(resp.Choices[0].Message.Content), &toolPrompt); err != nil {
 		if verbose {
-			color.Cyan("Unable to parse tool fr
+			color.Cyan("Unable to parse tool from prompts (%s), assuming got final answer", resp.Choices[0].Message.Co
