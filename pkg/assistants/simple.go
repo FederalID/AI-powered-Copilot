8000d78da5d8fedc315a193d6711f2d64394d8e2
FederalID <117157825+FederalID@users.noreply.github.com>
2022-08-20 16:38:43 +0000

    update version tag in docs

--- pkg/assistants/simple.go
+++ pkg/assistants/simple.go
@@ -83,4 +83,4 @@ func Assistant(model string, prompts []openai.ChatCompletionMessage, maxTokens i
 			if verbose {
 				color.Cyan("Invoking %s tool with inputs: \n============\n%s\n============\n\n", toolPrompt.Action.Name, toolPrompt.Action.Input)
 			}
-			ret, err := tools.Copilo
+			ret, err := tools.CopilotTools[toolPrompt.Action.Name](toolPrompt.Action.Input)
