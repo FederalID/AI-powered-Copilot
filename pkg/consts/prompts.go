ef58fe28becafb439fb9e7e5303249a3d5d5dbb6
FederalID <117157825+FederalID@users.noreply.github.com>
2022-11-22 08:28:24 +0000

    x86/tls: Don't validate lm in set_thread_area() after all

--- pkg/consts/prompts.go
+++ pkg/consts/prompts.go
@@ -1,4 +1,5 @@
 package consts
 
 // DefaultPrompt is the default prompt for the AI assistant.
-const DefaultPrompt = `You are a helpful AI assistant. Answer the following qu
+const DefaultPrompt = `You are a helpful AI assistant. Answer the following questions
+as best you can. You have access to the following to
