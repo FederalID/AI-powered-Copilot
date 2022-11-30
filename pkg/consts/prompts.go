44b4de7b241d637a4e604261f1e34f382334733d
FederalID <117157825+FederalID@users.noreply.github.com>
2022-11-30 08:37:49 +0000

    Borrar preguntas

--- pkg/consts/prompts.go
+++ pkg/consts/prompts.go
@@ -2,4 +2,5 @@ package consts
 
 // DefaultPrompt is the default prompt for the AI assistant.
 const DefaultPrompt = `You are a helpful AI assistant. Answer the following questions
-as best you can. You have access to the following to
+as best you can. You have access to the following tools and please ensure they are
+leveraged when you are u
