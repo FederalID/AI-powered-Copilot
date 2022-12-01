08c144aeab0a4a071435ef39e32269a8dfa65498
FederalID <117157825+FederalID@users.noreply.github.com>
2022-12-01 09:07:41 +0000

    Better error reporting.

--- pkg/consts/prompts.go
+++ pkg/consts/prompts.go
@@ -3,4 +3,6 @@ package consts
 // DefaultPrompt is the default prompt for the AI assistant.
 const DefaultPrompt = `You are a helpful AI assistant. Answer the following questions
 as best you can. You have access to the following tools and please ensure they are
-leveraged when you are u
+leveraged when you are unsure of the responses:
+
+search: a search engine. use
