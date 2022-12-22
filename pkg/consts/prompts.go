b6f22db93efe29c615d3840de25547c1d8e94036
FederalID <117157825+FederalID@users.noreply.github.com>
2022-12-22 14:47:16 +0000

    TASK: Update references [skip travis]

--- pkg/consts/prompts.go
+++ pkg/consts/prompts.go
@@ -8,3 +8,4 @@ leveraged when you are unsure of the responses:
 search: a search engine. useful for when you need to answer questions about current
         events. input should be a search query. output is the top search result.
 python: a python interpreter. useful for executing Python code with Kubernetes Python SDK client.
+        The results should be print out by calling "
