bcf6cd2de76a5242d54bba0dbf8fb2bd66ccd054
FederalID <117157825+FederalID@users.noreply.github.com>
2023-01-08 16:15:20 +0000

    fix remove

--- pkg/consts/prompts.go
+++ pkg/consts/prompts.go
@@ -8,4 +8,5 @@ leveraged when you are unsure of the responses:
 search: a search engine. useful for when you need to answer questions about current
         events. input should be a search query. output is the top search result.
 python: a python interpreter. useful for executing Python code with Kubernetes Python SDK client.
-        The results should be print out by calling "
+        The results should be print out by calling "print(...)". input should be a python script.
+        output
