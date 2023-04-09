e12155bfb8f44250ee7933406673676977085dad
FederalID <117157825+FederalID@users.noreply.github.com>
2023-04-09 16:41:56 +0000

    version 0.9.3

--- pkg/llms/tokens_test.go
+++ pkg/llms/tokens_test.go
@@ -70,4 +70,6 @@ func TestConstrictPrompt(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := ConstrictPrompt(tt.args.prompt, tt.args.model, tt.args.maxTokens); got != tt.want {
-		
+				t.Errorf("ConstrictPrompt() = %v, want %v", got, tt.want)
+			}
+		})
