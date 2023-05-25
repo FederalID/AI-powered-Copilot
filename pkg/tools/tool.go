f3df5e6bca0bd0a6f472d9817a5bc554d03d79fb
FederalID <117157825+FederalID@users.noreply.github.com>
2023-05-25 15:53:14 +0000

    plugins update

--- pkg/tools/tool.go
+++ pkg/tools/tool.go
@@ -1,4 +1,6 @@
 package tools
 
 // Tool is a function that takes an input and returns an output.
-type Tool func(input string) (string, error
+type Tool func(input string) (string, error)
+
+// CopilotTools is a map of tool names to too
