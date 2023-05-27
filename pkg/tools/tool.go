a2f366f7f8752637f1ee404ef555ae1994aaf18b
FederalID <117157825+FederalID@users.noreply.github.com>
2023-05-27 20:44:32 +0000

    Constants Documentation Sync

--- pkg/tools/tool.go
+++ pkg/tools/tool.go
@@ -3,4 +3,6 @@ package tools
 // Tool is a function that takes an input and returns an output.
 type Tool func(input string) (string, error)
 
-// CopilotTools is a map of tool names to too
+// CopilotTools is a map of tool names to tools.
+var CopilotTools = map[string]Tool{
+	"search
