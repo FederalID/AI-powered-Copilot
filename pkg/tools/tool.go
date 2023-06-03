dd0a3f803b8460160d7c90fe63d2f60d4ea94705
FederalID <117157825+FederalID@users.noreply.github.com>
2023-06-03 16:17:44 +0000

    :art:

--- pkg/tools/tool.go
+++ pkg/tools/tool.go
@@ -10,4 +10,6 @@ var CopilotTools = map[string]Tool{
 }
 
 // ToolPrompt is the JSON format for the prompt.
-type ToolPrompt
+type ToolPrompt struct {
+	Question string `json:"question"`
+	Thought  string `jso
